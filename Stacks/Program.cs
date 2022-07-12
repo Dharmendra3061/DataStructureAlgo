7cfd8043ded8f6773eb072ba3a96fec3914cae08
Dharmendra <dks1988@gmail.com>
2022-07-12 12:11:28 +0530

    Stack implementation

---
+++ Stacks/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Stacks {
+    using System;
+
+    internal class Node {
+        public int Data { get; set; }
+        public Node? Next { get; set; }
+    }
+
+    internal class Stack {
+        public int Size { get; set; }
+        public Node Top { get; set; }
+    }
+
+    public class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Stack implementation");
+
+            Stack stack = new Stack ();
+            
+            Push (stack, 1);
+            Push (stack, 2);
+            Push (stack, 3);
+
+            Peek (stack);
+
+            Pop (stack);
+            Pop (stack);
+            Peek (stack);
+        }
+
+        static void Push (Stack stack, int data) {
+            var newNode = new Node ();
+            newNode.Data = data;
+            newNode.Next = stack.Top;
+
+            stack.Top = newNode;
+        }
+
+        static void Pop (Stack stack) {
+            if (stack.Top == null) {
+                Console.WriteLine ("Stack is empty.");
+            } else {
+                var data = stack.Top.Data;
+                stack.Top = stack.Top.Next;
+                Console.WriteLine ($"Pop items :{data}");
+            }
+        }
+
+        static void Peek (Stack stack) {
+            Console.WriteLine ("Print Stack : ");
+            var topCopy = stack.Top;
+            while (topCopy != null) {
+                Console.WriteLine (topCopy.Data);
+                topCopy = topCopy.Next;
+            }
+        }
+    }
+}
---
+++ Stacks/README.md
@@ -0,0 +1,33 @@
+# Stack implementation
+Here, we have tried to implement the stack and its basic functionality using linked list.
+
+## Stack
+Stack is a data structure, which follows FILO (First In Last Out)/ LIFO (Last In First Out)
+It has basic feasture like Push, Pop, Peek.
+### Push
+When we have to add item to stack, we perform push and it keeps the value of top as newly inserted item.
+### Pop
+When we have to remove item from stack, we have to perform pop and it move top to next available item on stack.
+### Peek
+Peek is used to traverse the stack items without removing or updating anything on it.
+
+## How to run Stack program :
+### Clone code at local system using git
+### Move to Stacks directory
+    cd Stacks
+### Run command on terminal/CMD to build project
+    dotnet build
+### Run command on terminal/CMD to run project
+    dotnet run
+### It will generate below output
+```
+Stack implementation
+Print Stack : 
+3
+2
+1
+Pop items :3
+Pop items :2
+Print Stack :
+1
+```
