eedaed0d384e1b2b50c6506b188039b0555a9aad
Dharmendra <dks1988@gmail.com>
2023-09-04 11:16:57 +0530

    Moving features into Stack class

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -2,13 +2,39 @@
     using System;
 
     internal class Node {
-        public int Data { get; set; }
+        public string Data { get; set; }
         public Node? Next { get; set; }
     }
 
     internal class Stack {
         public int Size { get; set; }
-        public Node Top { get; set; }
+        public Node? Top { get; set; }
+
+        public void Push (string data) {
+            var newNode = new Node ();
+            newNode.Data = data;
+            newNode.Next = this.Top;
+
+            this.Top = newNode;
+        }
+
+        public string Pop () {
+            if (this.Top == null) {
+                return "Stack is Empty!!!";
+            } else {
+                string data = this.Top.Data;
+                this.Top = this.Top.Next;
+                return data;
+            }
+        }
+
+        public string Peek () {
+            if (this.Top == null) {
+                return "Stack is Empty!!!";
+            } else {
+                return this.Top.Data;
+            }
+        }
     }
 
     public class Program {
@@ -17,42 +43,15 @@
 
             Stack stack = new Stack ();
             
-            Push (stack, 1);
-            Push (stack, 2);
-            Push (stack, 3);
+            stack.Push ("A");
+            stack.Push ("B");
+            stack.Push ("C");
 
-            Peek (stack);
+            Console.WriteLine(stack.Peek());
 
-            Pop (stack);
-            Pop (stack);
-            Peek (stack);
-        }
-
-        static void Push (Stack stack, int data) {
-            var newNode = new Node ();
-            newNode.Data = data;
-            newNode.Next = stack.Top;
-
-            stack.Top = newNode;
-        }
-
-        static void Pop (Stack stack) {
-            if (stack.Top == null) {
-                Console.WriteLine ("Stack is empty.");
-            } else {
-                var data = stack.Top.Data;
-                stack.Top = stack.Top.Next;
-                Console.WriteLine ($"Pop items :{data}");
-            }
-        }
-
-        static void Peek (Stack stack) {
-            Console.WriteLine ("Print Stack : ");
-            var topCopy = stack.Top;
-            while (topCopy != null) {
-                Console.WriteLine (topCopy.Data);
-                topCopy = topCopy.Next;
-            }
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
         }
     }
 }
